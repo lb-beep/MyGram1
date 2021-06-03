5b0a93a3ba7ffe29a2139e5f7d21b09cfe7f8116
Luis <lbermudez530@southwestuniversity.edu>
2021-06-02 19:47:33 -0600

    first commit

---
+++ MyGram/wwwroot/js/Index.js
@@ -0,0 +1,31 @@
+﻿var divContainer = $("MainContainer");
+
+$("<span></span>").text("My Gram").css("margin-right", "10px").appendTo(divContainer);
+$("<button></button>").text("Log In").appendTo(divContainer);
+$("<button></button>").text("Sign Up").appendTo(divContainer);
+
+$("<br>").appendTo(divContainer);
+$("<br>").appendTo(divContainer);
+
+$("<span></span>").text("Image 1").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span>").text("Image 2").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span>").text("Image 3").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span>").text("Image 4").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span>").text("Image 5").css("margin-right", "10px").appendTo(divContainer);
+
+$("<br>").appendTo(divContainer);
+$("<br>").appendTo(divContainer);
+
+$("<span></span>").text("About").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span>").text("Blog").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span>").text("Jobs").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span>").text("Help").css("margin-right", "10px").appendTo(divContainer);
+
+$("<br>").appendTo(divContainer);
+
+$("<span></span>").text("English").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span>").text("2021").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span>").text("MyGram").css("margin-right", "10px").appendTo(divContainer);
+
+
+

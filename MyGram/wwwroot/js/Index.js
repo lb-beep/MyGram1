fdc6107a1ff6a48af22255dbbe390b5338c151e8
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-06-02 19:56:31 -0600

    Update Index.js

--- MyGram/wwwroot/js/Index.js
+++ MyGram/wwwroot/js/Index.js
@@ -1,8 +1,8 @@
-﻿var divContainer = $("MainContainer");
+var divContainer = $("MainContainer");
 
 $("<span></span>").text("My Gram").css("margin-right", "10px").appendTo(divContainer);
-$("<button></button>").text("Log In").appendTo(divContainer);
-$("<button></button>").text("Sign Up").appendTo(divContainer);
+$("<button></button>").attr("id","LogIn").text("Log In").appendTo(divContainer);
+$("<button></button>").attr("id", "SignUp").text("Sign Up").appendTo(divContainer);
 
 $("<br>").appendTo(divContainer);
 $("<br>").appendTo(divContainer);
@@ -27,5 +27,13 @@ $("<span></span>").text("English").css("margin-right", "10px").appendTo(divConta
 $("<span></span>").text("2021").css("margin-right", "10px").appendTo(divContainer);
 $("<span></span>").text("MyGram").css("margin-right", "10px").appendTo(divContainer);
 
+$("#LogIn").click(function () {
+    alert("You have logged in")
+});
 
+$("#SignUP").click(function () {
+    $("<span></span>").text("User").css("margin-right", "10px").appendTo(divContainer);
+    $("<span></span>").text("Password").css("margin-right", "10px").appendTo(divContainer);
+
+});
 
